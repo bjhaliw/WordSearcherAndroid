bfdf6b15976174c978efe0b5672de14fa833c34c
Brenton Haliw <45081242+bjhaliw@users.noreply.github.com>
2021-09-25 21:31:26 -0400

    Redesigned entire app

--- app/src/main/java/com/example/wordsearcher/MainActivity.java
+++ app/src/main/java/com/example/wordsearcher/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.wordsearcher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +11,14 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
+
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,18 +28,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Open up the text file containing the words to search
         InputStream stream = getResources().openRawResource(R.raw.words_alpha);
 
         // Word Search object, will perform the word searching
         WordSearcher searcher = new WordSearcher(stream);
 
-        // The EditText fields containing user input
-        EditText startsWith = (EditText) findViewById(R.id.startsWithTextField);
-        EditText containsExact = (EditText) findViewById(R.id.containsExactTextField);
-        EditText containsLetters = (EditText) findViewById(R.id.containsLettersTextField);
-        EditText containsOnly = (EditText) findViewById(R.id.onlyContainsTextField);
-        EditText endsWith = (EditText) findViewById(R.id.endsWithTextField);
-        EditText wordLength = (EditText) findViewById(R.id.wordLengthTextField);
+        // TextFields for user input
+        TextInputEditText letters = (TextInputEditText) findViewById(R.id.lettersFieldText);
+        TextInputEditText starts = (TextInputEditText) findViewById(R.id.startsWithFieldText);
+        TextInputEditText contains = (TextInputEditText) findViewById(R.id.containsExactFieldText);
+        TextInputEditText ends = (TextInputEditText) findViewById(R.id.endsWithFieldText);
+        TextInputEditText length = (TextInputEditText) findViewById(R.id.worldLengthFieldText);
 
         // Search Button to start the process
         Button searchButton = (Button) findViewById(R.id.searchButton);
@@ -50,53 +58,67 @@ public class MainActivity extends AppCompatActivity {
                 // Don't need to do anything since the keyboard is already hidden
             }
 
-
             // Convert wordLength EditText field from String to int
-            String lengthString = wordLength.getText().toString();
-            int length;
+            String lengthString = length.getText().toString();
+            int numLength;
 
             try {
-                length = Integer.parseInt(wordLength.getText().toString());
+                numLength = Integer.parseInt(lengthString);
             } catch (NumberFormatException e2) {
-                length = 0;
+                numLength = 0;
             }
 
-            if (startsWith.getText().toString().equals("")
-                    && containsExact.getText().toString().equals("")
-                    && containsLetters.getText().toString().equals("")
-                    && containsOnly.getText().toString().equals("")
-                    && endsWith.getText().toString().equals("")
-                    && wordLength.getText().toString().equals("")) {
+            String lettersString = letters.getText().toString();
+            String startsString = starts.getText().toString();
+            String containsString = contains.getText().toString();
+            String endsString = ends.getText().toString();
 
-                System.out.println("No input");
+            // Make sure at least one field was entered
+            if (!lettersString.matches("") || !startsString.matches("") || !containsString.matches("") || !endsString.matches("") || !lengthString.matches("")) {
 
-            } else {
-                String result = searcher.findWord(
-                        startsWith.getText().toString().trim(),
-                        containsExact.getText().toString().trim(),
-                        containsLetters.getText().toString().trim(),
-                        containsOnly.getText().toString().trim(),
-                        endsWith.getText().toString().trim(),
-                        length);
+                searcher.findWord(letters.getText().toString(), starts.getText().toString(), contains.getText().toString(), ends.getText().toString(), numLength);
 
-                String[] resultSplit = result.split("\n");
+            }
 
-                LinearLayout layout = new LinearLayout(this);
-                layout.setOrientation(LinearLayout.VERTICAL);
+            String[] split = searcher.getSb().toString().split("\n");
+            TreeMap<Integer, ArrayList<String>> map = searcher.getTreeMap();
 
-                for (String s : resultSplit) {
+            System.out.println(map);
+
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+
+            if (map.size() == 0) {
+                for (String s : split) {
                     TextView textView = new TextView(this);
                     textView.setText(s);
                     layout.addView(textView);
                 }
+            } else {
+                System.out.println("Looking at the tree map");
+                for (Map.Entry<Integer, ArrayList<String>> value : map.entrySet()) {
+                    TextView num = new TextView(this);
+                    num.setText("Words of Length: " + value.getKey());
+                    layout.addView(num);
+                    for (String s : value.getValue()) {
+                        System.out.println("Curr Word: " + s);
+                        TextView word = new TextView(this);
+                        word.setText(s);
+                        layout.addView(word);
+                    }
 
-                view.removeAllViews();
-
-                view.addView(layout);
-
-                view.scrollTo(0, 0);
+                    TextView space = new TextView(this);
+                    space.setText("\n");
+                    layout.addView(space);
+                }
             }
 
+
+            view.removeAllViews();
+
+            view.addView(layout);
+
+            view.scrollTo(0, 0);
 
         });
 
--- app/src/main/java/com/example/wordsearcher/WordSearcher.java
+++ app/src/main/java/com/example/wordsearcher/WordSearcher.java
@@ -4,6 +4,8 @@ import android.content.Context;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.TreeMap;
@@ -11,41 +13,75 @@ import java.util.TreeMap;
 public class WordSearcher {
 
     // Key: A-Z, Value: Words starting with Key letter
-    private HashMap<String, ArrayList<String>> wordMap;
+
     private TreeMap<Integer, ArrayList<String>> treeMap;
+    private StringBuilder sb;
+    private HashMap<String, ArrayList<String>> startsWithMap;
+    private HashMap<String, ArrayList<String>> endsWithMap;
     private ArrayList<String> wordList;
 
     public WordSearcher(InputStream stream) {
-        //wordMap = new HashMap<>();
         this.wordList = new ArrayList<>();
+        this.startsWithMap = new HashMap<>();
+        this.endsWithMap = new HashMap<>();
         loadWordFile(stream);
     }
 
-    public String findWord(String startsWith, String containsExact, String containsLetters,
-                           String containsOnly, String endsWith, int wordLength) {
+    /**
+     * Main search function for the WordSearcher class. Loops through the entire word list if
+     * the user did not supply any of the TextFields.
+     * @param letters
+     * @param startsWith
+     * @param contains
+     * @param endsWith
+     * @param wordLength
+     */
+    public void findWord(String letters, String startsWith, String contains,
+                         String endsWith, int wordLength) {
 
-        StringBuilder sb = new StringBuilder();
+        this.treeMap = new TreeMap<>();
+        this.sb = new StringBuilder();
+
+        System.out.println("Letters: Word Search: " + letters);
+        System.out.println("Length " + wordLength);
+
         String currWord;
 
+        // Iterate through the word list
         for (int i = 0; i < this.wordList.size(); i++) {
             currWord = this.wordList.get(i);
 
-            if (currWord.startsWith(startsWith) && currWord.contains(containsExact) &&
-                    doesWordContainLetters(currWord, containsLetters) && currWord.endsWith(endsWith)) {
+            if (doesWordContainLetters(currWord, letters) && currWord.startsWith(startsWith)
+                    && currWord.contains(contains) && currWord.endsWith(endsWith)) {
 
-                sb.append(currWord + "\n");
+                addWordToLists(currWord, wordLength);
             }
         }
-
-        return sb.toString();
     }
 
+    /**
+     * Helper method to determine if a word contains some letters in arbitrary positions. Can contain
+     * duplicate letters.
+     *
+     * @param currWord        - The current word to check
+     * @param containsLetters - The specified letters that may be in the word in any order
+     * @return
+     */
     private boolean doesWordContainLetters(String currWord, String containsLetters) {
+
+        // User didn't include this field, automatically return true
         if (containsLetters.length() == 0) {
             return true;
         }
 
+        // Can't have a word larger than the number of given letters
+        if (currWord.length() > containsLetters.length()) {
+            return false;
+        }
+
+        // Split the the current word into an array
         String[] arr = currWord.split("");
+
         String tempLetters = containsLetters;
         int counter = 0;
 
@@ -54,6 +90,8 @@ public class WordSearcher {
             if (tempLetters.contains(arr[i])) {
                 tempLetters = tempLetters.replaceFirst(arr[i], "");
                 counter++;
+            } else {
+                return false;
             }
 
             if (counter == containsLetters.length()) {
@@ -61,22 +99,83 @@ public class WordSearcher {
             }
         }
 
+        if (counter > 0 && counter <= containsLetters.length()) {
+            return true;
+        }
+
         return false;
     }
 
+
     /**
-     * Loads the HashMaps containing the word list in the res/raw/alpha_words.txt file
+     * Helper method to either add the current word to a TreeMap if the length is 0, or append
+     * it to the StringBuilder if a length is specified
+     *
+     * @param currWord - The current word to add to the running track of words
+     * @param length   - The specified target length of the word
+     */
+    private void addWordToLists(String currWord, int length) {
+
+        // User didn't specify a length, sort by length
+        if (length == 0) {
+            if (treeMap.containsKey(currWord.length())) {
+                treeMap.get(currWord.length()).add(currWord);
+            } else {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(currWord);
+                treeMap.put(currWord.length(), list);
+            }
+        } else {
+            if (currWord.length() == length) {
+                this.sb.append(currWord + "\n");
+            }
+        }
+    }
+
+    /**
+     * Loads the ArrayList<String> containing the word list in the res/raw/alpha_words.txt file
      *
      * @param stream - InputStream to the text file location
      */
     private void loadWordFile(InputStream stream) {
         Scanner scanner = new Scanner(stream);
+        String currWord, firstLetter, lastLetter;
 
         while (scanner.hasNextLine()) {
-            this.wordList.add(scanner.nextLine());
+            currWord = scanner.nextLine();
+            firstLetter = currWord.substring(0,1);
+            lastLetter = currWord.substring(currWord.length()-1);
+
+            // Add current word to overall list
+            this.wordList.add(currWord);
+
+//            // Add word to Starts With map
+//            if (startsWithMap.containsKey(firstLetter)) {
+//                startsWithMap.get(firstLetter).add(currWord);
+//            } else {
+//                ArrayList<String> list = new ArrayList<>();
+//                list.add(currWord);
+//                startsWithMap.put(firstLetter, list);
+//            }
+//
+//            // Add word to Ends With map
+//            if (endsWithMap.containsKey(lastLetter)) {
+//                endsWithMap.get(lastLetter).add(currWord);
+//            } else {
+//                ArrayList<String> list = new ArrayList<>();
+//                list.add(currWord);
+//                endsWithMap.put(lastLetter, list);
+//            }
         }
 
         scanner.close();
     }
 
+    public TreeMap<Integer, ArrayList<String>> getTreeMap() {
+        return this.treeMap;
+    }
+
+    public StringBuilder getSb() {
+        return sb;
+    }
 }
